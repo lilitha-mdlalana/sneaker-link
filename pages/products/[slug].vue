aa9db86d746a3ccc8dec9a7fa7225aceb2afc3fd
Lilitha Mdlalana <jranonymous517@gmail.com>
2022-09-23 11:47:07 +0200

    Add: Code for product page

--- pages/products/[slug].vue
+++ pages/products/[slug].vue
@@ -3,10 +3,31 @@ import { useShoeStore } from "~/store/shoes";
 
 const route = useRoute()
 const shoes = useShoeStore()
-
-console.log(shoes.getShoeById(route.params.slug))
+shoes.getShoes()
+const product = shoes.getShoeById(route.params.slug)
+console.log(product)
 </script>
 
 <template>
-  <h1>sneaker at {{ $route.params.slug }}</h1>
+  <div class="container my-4">
+    <div class="columns is-half-desktop">
+      <div>
+        <figure>
+          <img :src="product.media.smallImageUrl" class="image" alt="">
+        </figure>
+      </div>
+      <div class="is-primary">
+        <h1 class="is-4">{{product.shoe}}</h1>
+        ${{product.retailPrice}}<br/>
+        {{product.brand}} | {{product.name}}<br/>
+        {{product.gender}}<br/>
+        Released : {{product.releaseDate}}<br/>
+        <hr>
+        <button class="button is-link">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
+
